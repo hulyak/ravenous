cea5dbc1cb23f5711db70e294afd459ae52fbdad
hulyak <57311842+hulya95@users.noreply.github.com>
2020-05-06 20:27:13 -0700

    create searchBar component using Yelp Api

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+	'Best Match': 'best_match',
+	'Highest Rated': 'rating',
+	'Most Reviewed': 'review_count'
+};
+
+class SearchBar extends React.Component {
+	renderSortByOptions() {
+		// .To iterate through the object,start by accessing the keys of the sortByOptionsobject. Call the keys() method on the JavaScript Object library. Pass in sortByOptions as the argument.
+		return Object.keys(sortByOptions).map((sortByOption) => {
+			let sortByOptionValue = sortByOptions[sortByOption];
+			return <li key={sortByOptionValue}> {sortByOption} </li>;
+		});
+	}
+	render() {
+		return (
+			<div className="SearchBar">
+				<div className="SearchBar-sort-options">
+					<ul>
+						<renderSortByOptions />
+					</ul>
+				</div>
+				<div className="SearchBar-fields">
+					<input placeholder="Search Businesses" />
+					<input placeholder="Where?" />
+				</div>
+				<div className="SearchBar-submit">
+					<a>Let's Go</a>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
