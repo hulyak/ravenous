8eb4df2a1f9448af44217118699d5c2f31bbe107
hulyak <57311842+hulya95@users.noreply.github.com>
2020-05-08 14:04:27 -0700

    pass information to components

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './SearchBar.css';
 
 // A SearchBar component to search for business
+//https://www.yelp.com/developers/documentation/v3/business_search
 const sortByOptions = {
 	'Best Match': 'best_match',
 	'Highest Rated': 'rating',
@@ -9,8 +10,14 @@ const sortByOptions = {
 };
 
 class SearchBar extends React.Component {
+	// dynamically create the list items needed to display the sort options
+	// to help future proof against potential changes to the Yelp API.
+	//The method should iterate through the keys and values of the sortByOptions object and return a list item.
+	//The list item elements should use the keys as an attribute, and the values as content.
 	renderSortByOptions() {
-		// .To iterate through the object,start by accessing the keys of the sortByOptionsobject. Call the keys() method on the JavaScript Object library. Pass in sortByOptions as the argument.
+		// To iterate through the object,start by accessing the keys of the sortByOptions object.
+		//Call the keys() method on the JavaScript Object library. Pass in sortByOptions as the argument.
+		//Now that you have access to the keys, you’ll iterate through them using the map() method.
 		return Object.keys(sortByOptions).map((sortByOption) => {
 			let sortByOptionValue = sortByOptions[sortByOption];
 			return <li key={sortByOptionValue}> {sortByOption} </li>;
@@ -27,7 +34,7 @@ class SearchBar extends React.Component {
 					<input placeholder="Where?" />
 				</div>
 				<div className="SearchBar-submit">
-					<a>Let's Go</a>
+					<a href="">Let's Go</a>
 				</div>
 			</div>
 		);
