ce28dd250e12cdd535ece6764b650b25ed2c1096
hulyak <57311842+hulya95@users.noreply.github.com>
2020-06-29 22:23:44 -0700

    feat : retrieve the search results from Yelp Api and export the class

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,2 +1,31 @@
 const apiKey = 'hzkML4S-4IZV-EtajLaNGe7fZj_TnYk4OPaZyHJCpQ2Om-suEF85OuumfO9pNjOgMi6WEOI2Svi8O_QY5pKlitR_d3XS0yjK5A4BzPyRlo4o1BrUIdCRmlHwhr76XnYx';
-fetch()
+const Yelp = {
+  search(term, location, sortBy){
+    //  return a promise that will ultimately resolve to our list of businesses.
+   return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, 
+   {
+    headers : {
+      Authorization : `Bearer ${apiKey}`
+    }
+   }) //fetch /businesses endpoint
+   .then((response) => response.json())
+   .then((jsonResponse) => {
+     if (jsonResponse.businesses) {
+       return jsonResponse.businesses.map(business => ({
+         id : business.id,
+         imageSrc: business.image_url,
+         name: business.name,
+         address : business.location.address1,
+         city : business.location.city,
+         state : business.location.state,
+         zipCode : business.location.zip_code,
+         category : business.categories[0].title,
+         rating : business.rating,
+         reviewCount : business.review_count
+       }));
+      }
+   });
+  }
+};
+
+export default Yelp;
