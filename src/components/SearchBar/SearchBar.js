47956d202fc1ffda066ef4bb4596dcaebf323b3b
hulyak <57311842+hulya95@users.noreply.github.com>
2020-06-29 21:13:03 -0700

    feat : add visual feedback for the sorting options with css, set the state of the sorting options and input elements

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,13 +3,58 @@ import './SearchBar.css';
 
 // A SearchBar component to search for business
 //https://www.yelp.com/developers/documentation/v3/business_search
-const sortByOptions = {
-	'Best Match': 'best_match',
-	'Highest Rated': 'rating',
-	'Most Reviewed': 'review_count'
-};
 
 class SearchBar extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			term : "",
+			location : "",
+			sortBy : "best_match"
+		};  //initial state
+		this.sortByOptions = {
+			'Best Match': 'best_match',
+			'Highest Rated': 'rating',
+			'Most Reviewed': 'review_count'
+		};
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleSearch = this.handleSearch.bind(this);
+	}
+
+
+	// returns the current CSS class for a sorting option
+	//This will conditionally style each sort by option, displaying to the user which sorting option is currently selected
+	getSortByClass(sortByOption){
+		if (this.state.sortBy === sortByOption){
+			return 'active'; //add active class
+		}
+		return "";
+	}
+
+	// sets the state of a sorting option
+	handleSortByChange(sortByOption){
+		this.setState({
+			sortBy : sortByOption
+		});
+	}
+
+	// triggers event, input element
+	handleTermChange(event){
+		this.setState({
+     term : event.target.value
+		});
+	}
+	handleLocationChange(event){
+		this.setState({
+			location : event.target.value
+		});
+	}
+
+	handleSearch(event){
+	this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy); //props change the state values
+	event.preventDefault();
+	}
 	// dynamically create the list items needed to display the sort options
 	// to help future proof against potential changes to the Yelp API.
 	//The method should iterate through the keys and values of the sortByOptions object and return a list item.
@@ -18,9 +63,9 @@ class SearchBar extends React.Component {
 		// To iterate through the object,start by accessing the keys of the sortByOptions object.
 		//Call the keys() method on the JavaScript Object library. Pass in sortByOptions as the argument.
 		//Now that you have access to the keys, you’ll iterate through them using the map() method.
-		return Object.keys(sortByOptions).map((sortByOption) => {
-			let sortByOptionValue = sortByOptions[sortByOption]; //get the value 
-			return <li key={sortByOptionValue}> {sortByOption} </li>;
+		return Object.keys(this.sortByOptions).map((sortByOption) => {
+			let sortByOptionValue = this.sortByOptions[sortByOption]; //get the value 
+			return <li key={sortByOptionValue} className={this.getSortByClass(sortByOptionValue)} onClick={this.handleSortByChange.bind(this, sortByOptionValue)}> {sortByOption} </li>;
 		});
 	}
 
@@ -32,11 +77,11 @@ class SearchBar extends React.Component {
 					<ul>{this.renderSortByOptions()}</ul>
 				</div>
 				<div className="SearchBar-fields">
-					<input placeholder="Search Businesses" />
-					<input placeholder="Where?" />
+					<input placeholder="Search Businesses" onChange={this.handleTermChange}/>
+					<input placeholder="Where?" onChange={this.handleLocationChange}/>
 				</div>
 				<div className="SearchBar-submit">
-					<a href="">Let's Go</a>
+					<a  onClick={this.handleSearch}>Let's Go</a>
 				</div>
 			</div>
 		);
